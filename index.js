e9e6d607bd7663afa258ce492684a306f807debd
ThallesSantana <thalles.gonalves@eaportal.org>
2023-11-25 14:39:12 -0300

    criação - primeira view do projeto

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const exphbs = require("express-handlebars")
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'Handlebars')
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
-    resposta.send("Olá, mundo!")
+    resposta.render('home')
 })
 
 app.listen(3000, () => {
